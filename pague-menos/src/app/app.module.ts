55e34c706ae631879a3ed0bd4ed44cc47dd8d987
Leo <45577227+leozanchett@users.noreply.github.com>
2022-04-04 22:44:36 -0300

    feat: HttpClientModule

--- pague-menos/src/app/app.module.ts
+++ pague-menos/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeProductsComponent } from './home-products/home-products.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
