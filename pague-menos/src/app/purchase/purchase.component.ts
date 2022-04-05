01f3cd3ed1429b687970df7049b5e36cbc0301c6
Leo <45577227+leozanchett@users.noreply.github.com>
2022-04-05 13:35:19 -0300

    style: purchase screen

--- pague-menos/src/app/purchase/purchase.component.ts
+++ pague-menos/src/app/purchase/purchase.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from '../models/product';
+import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-purchase',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PurchaseComponent implements OnInit {
 
-  constructor() { }
+  product = {} as Product;
+
+  constructor(
+    private productService: ProductsService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    console.log(this.product);
+    console.log(JSON.stringify(this.product));
+    this.productService.postProduct(this.product);
+  }
+
+  backHome(): void {
+    this.router.navigate(['/']);
   }
 
 }
