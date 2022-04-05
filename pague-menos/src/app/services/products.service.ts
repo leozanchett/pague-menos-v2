ae0fbb3b0f721451bd14cf88c2dd011e8d8742a9
Leo <45577227+leozanchett@users.noreply.github.com>
2022-04-04 22:10:02 -0300

    feat: getAllProducts

--- pague-menos/src/app/services/products.service.ts
+++ pague-menos/src/app/services/products.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { catchError, Observable, of, tap } from 'rxjs';
+import { Product } from '../models/product';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  private url = 'http://localhost:8080/products';
+  private products: Product[] = [];
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  //headers
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json'
+  });
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.url).
+      pipe(
+        tap(_ => this.log('fetched products')),
+        catchError(this.handleError('getProducts', []))
+      );
+  }
+
+  private log(message: String) {
+    console.log(`Products Service: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    };
+  }
+
+
 }
